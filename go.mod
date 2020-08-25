709a019ac1458762e3adede622232810eb33f268
tyasrus <nantotyas@gmail.com>
2020-08-25 16:34:14 +0700

    go mod init and add gosoap lib

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tyasrush/go-learn
+
+go 1.14
+
+require github.com/tiaguinho/gosoap v1.4.3
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/xml"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/tiaguinho/gosoap"
+)
+
+// GetIPLocationResponse will hold the Soap response
+type GetIPLocationResponse struct {
+	GetIPLocationResult string `xml:"GetIpLocationResult"`
+}
+
+// GetIPLocationResult will
+type GetIPLocationResult struct {
+	XMLName xml.Name `xml:"GeoIP"`
+	Country string   `xml:"Country"`
+	State   string   `xml:"State"`
+}
+
+var (
+	r GetIPLocationResponse
+)
+
+func main() {
+	httpClient := &http.Client{
+		Timeout: 1500 * time.Millisecond,
+	}
+	soap, err := gosoap.SoapClient("http://wsgeoip.lavasoft.com/ipservice.asmx?WSDL", httpClient)
+	if err != nil {
+		log.Fatalf("SoapClient error: %s", err)
+	}
+
+	// Use gosoap.ArrayParams to support fixed position params
+	params := gosoap.Params{
+		"sIp": "8.8.8.8",
+	}
+
+	res, err := soap.Call("GetIpLocation", params)
+	if err != nil {
+		log.Fatalf("Call error: %s", err)
+	}
+
+	res.Unmarshal(&r)
+
+	// GetIpLocationResult will be a string. We need to parse it to XML
+	result := GetIPLocationResult{}
+	err = xml.Unmarshal([]byte(r.GetIPLocationResult), &result)
+	if err != nil {
+		log.Fatalf("xml.Unmarshal error: %s", err)
+	}
+
+	if result.Country != "US" {
+		log.Fatalf("error: %+v", r)
+	}
+
+	log.Println("Country: ", result.Country)
+	log.Println("State: ", result.State)
+}
