22329ac6304195ef7342c5a3b0a0a29a785d7eaa
tyasrush <nantotyas@gmail.com>
2021-12-29 23:49:22 +0700

    adding simple server for rest api

---
+++ simple-server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+)
+
+type (
+	Config struct {
+		Server Server `yaml:"server"`
+	}
+
+	Server struct {
+		Port string `yaml:"port"`
+	}
+)
+
+var configFilePath string
+
+func parsePath() {
+	flag.StringVar(&configFilePath, "cpath", "", "config file path")
+	flag.Parse()
+}
+
+func main() {
+	parsePath()
+	configFile, err := ioutil.ReadFile(configFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	var cfg Config
+	if err = yaml.Unmarshal(configFile, &cfg); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("listen port %s\n", cfg.Server.Port)
+	http.ListenAndServe(cfg.Server.Port, nil)
+}
