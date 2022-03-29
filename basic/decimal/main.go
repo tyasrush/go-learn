5d6c2e178e7c071760ab1ef93c1df04214623a18
tyas <tyas@mesinrumput>
2022-03-29 11:44:12 +0700

    adding some examples

--- basic/decimal/main.go
+++ basic/decimal/main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 
 	"github.com/shopspring/decimal"
-	"github.com/tyasrush/go-learn/basic/decimal/example"
 )
 
 func main() {
-	example.TestCalculateDecimal()
+	// example.TestCalculateDecimal()
 
-	zeroError := decimal.NewFromFloat(float64(0.1)).Div(decimal.Zero)
-	fmt.Println("zero error : ", zeroError)
+	// zeroError := decimal.NewFromFloat(float64(0.1)).Div(decimal.Zero)
+	// fmt.Println("zero error : ", zeroError)
+
+	roundDec := decimal.NewFromFloat(0.989797).Round(2)
+	fmt.Println("round testing: ", roundDec)
+
+	testingDiv := decimal.Zero.Mul(decimal.Zero).Round(2)
+	fmt.Println("zero ", testingDiv)
 }
