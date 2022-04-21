21215d33da0f7e5c3ac893114e82760409ecc318
tyasrush <nantotyas@gmail.com>
2022-04-21 12:42:30 +0700

    add bfs implementation

---
+++ basic/bfs/main.go
@@ -0,0 +1,97 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Val   int
+	Right *Node
+	Left  *Node
+	Next  *Node
+}
+
+type NodeLevel struct {
+	node  *Node
+	level int
+}
+
+func bfs(root *Node) *Node {
+	// root checking
+	if root == nil {
+		return root
+	}
+
+	queue := []NodeLevel{
+		{
+			root,
+			0,
+		},
+	}
+	visited := []*Node{}
+
+	for len(queue) > 0 {
+		vertex := queue[0]
+		node, level := vertex.node, vertex.level
+		visited = append(visited, node)
+		queue = queue[1:]
+
+		if node.Left != nil {
+			leftNode := NodeLevel{
+				node:  node.Left,
+				level: level + 1,
+			}
+
+			queue = append(queue, leftNode)
+		}
+
+		if node.Right != nil {
+			rightNode := NodeLevel{
+				node:  node.Right,
+				level: level + 1,
+			}
+
+			queue = append(queue, rightNode)
+		}
+	}
+	for _, v := range visited {
+		fmt.Printf("all visited node: %v\n", v.Val)
+	}
+
+	return root
+}
+
+func main() {
+
+	node1 := &Node{
+		Val: 1,
+		Left: &Node{
+			Val: 2,
+			Left: &Node{
+				Val: 3,
+				Right: &Node{
+					Val: 7,
+					Right: &Node{
+						Val: 8,
+					},
+				},
+			},
+			Right: &Node{
+				Val: 4,
+				Left: &Node{
+					Val: 9,
+				},
+				Right: &Node{
+					Val: 10,
+				},
+			},
+		},
+		Right: &Node{
+			Val: 5,
+			Right: &Node{
+				Val: 6,
+			},
+		},
+	}
+
+	bfs(node1)
+
+}
--- basic/ladderword/main.go
+++ basic/ladderword/main.go
@@ -1,1 +1,8 @@
 package main
+
+func shortestPath(beginWord, endWord string, wordList []string) int {
+	return 0
+}
+
+func main() {
+}
